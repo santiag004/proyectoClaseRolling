f2dcca588730c32165b1d3356dec9cf70cf43ac1
lourdesreyna <reynalourdes91@gmail.com>
2023-12-20 10:25:16 -0300

    tarea eliminar

--- javaScript/delete.js
+++ javaScript/delete.js
@@ -1,7 +1,6 @@
 const buttonEliminar = document.createElement("button")
 buttonEliminar.textContent = "Eliminar"
 buttonEliminar.addEventListener("click", () => {
-    
 })
 
 function productoAeliminar (ulProductos , productoAeliminar) {
