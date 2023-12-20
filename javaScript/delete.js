aba38c858df62bbcf4f015473022f342b2774c1c
lourdesreyna <reynalourdes91@gmail.com>
2023-12-20 10:03:29 -0300

    boton eliminar

--- javaScript/delete.js
+++ javaScript/delete.js
@@ -1,8 +1,12 @@
 const buttonEliminar = document.createElement("button")
 buttonEliminar.textContent = "Eliminar"
 buttonEliminar.addEventListener("click", () => {
+    
 })
 
-function productoAeliminar (productoAeliminar) {
-
+function productoAeliminar (ulProductos , productoAeliminar) {
+const eliminar = ulProductos.indexOF(productoAeliminar)
+ if (eliminar !== -1) {
+    ulProductos.splice(eliminar , 1)
+}
 }
