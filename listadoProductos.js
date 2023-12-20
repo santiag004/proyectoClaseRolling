a3fc8f3f8476b674d29e58a63aee11077f135e99
Santiago Soraire <eneasvallejo04@gmail.com>
2023-12-20 09:33:12 -0300

    Avance en js

---
+++ listadoProductos.js
@@ -0,0 +1,9 @@
+const database = window.localStorage
+const ulProductos = document.getElementById("listaProductos")
+
+let productosPrueba = {
+    nombre: "Campera",
+    descripcion: "Campera abrigada para bajas temperaturas",
+    precio: "$4000",
+}
+
