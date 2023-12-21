2d534d84b7a1ca8de2c6a0692375573d3f2ac51b
lourdesreyna <reynalourdes91@gmail.com>
2023-12-21 11:11:52 -0300

    eliminar productos de la lista

--- javaScript/delete.js
+++ javaScript/delete.js
@@ -1,6 +1,8 @@
 const buttonEliminar = document.createElement("button")
 buttonEliminar.textContent = "Eliminar"
 buttonEliminar.addEventListener("click", () => {
+    productoAeliminar(ulProductos, productoSeleccionado)
+    //falta llamar a la funcion y agregarle a ulProducto el removechild(li)
 })
 
 function productoAeliminar (ulProductos , productoAeliminar) {
@@ -9,3 +11,6 @@ const eliminar = ulProductos.indexOF(productoAeliminar)
     ulProductos.splice(eliminar , 1)
 }
 }
+//al li tengo que agregarle el appenchild (buttonEliminar)
+//ulProductos agregarle el appenchild (li)
+//localStorage?????? removeIteam pero que parametro paso? lista? 
