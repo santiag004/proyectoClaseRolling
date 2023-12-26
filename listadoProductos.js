edc60673e8edbc06f910a8c8cf02d92ea07f811e
facundo <facurmedina@gmail.com>
2023-12-26 11:38:08 -0300

    Función listar productos

--- listadoProductos.js
+++ listadoProductos.js
@@ -7,3 +7,23 @@ let productosPrueba = {
     precio: "$4000",
 }
 
+const listarProductos = () => {
+    ulProductos.innerHTML = ""
+    let idProductos = Object.keys(database)
+    for (id of idProductos) {
+        let productos = JSON.parse(database.getItem(id))
+        const li = document.createElement("li")
+        li.textContent = `NOMBRE: ${productos.nombre} -DESCRIPCIÓN: ${productos.detalle} - PRECIO: ${productos.precio}`
+        ulProductos.appendChild(li)
+        const botonEliminarProductos = document.createElement("button")
+        botonEliminarProductos.textContent = "Borrar del carrito"
+        li.appendChild(botonEliminarProductos)
+        botonEliminarProductos.addEventListener ("click", () => {
+            eliminarProducto(database, productos)
+            listarProductos()
+            espacioLibre()
+            carritoLleno()
+        })
+    }
+}
+listarProductos()
