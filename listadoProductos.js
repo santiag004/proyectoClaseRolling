322167d1ff4bc5c716897793997209d4ca98e3b2
Santiago Soraire <eneasvallejo04@gmail.com>
2023-12-26 22:02:14 -0300

    Commit funcion Listar

--- listadoProductos.js
+++ listadoProductos.js
@@ -7,3 +7,21 @@ let productosPrueba = {
     precio: "$4000",
 }
 
+const listarProductos = () => {
+    ulProductos.innerHTML = ""
+    let idProductos = Object.keys(database)
+    for (id of idProductos) {
+        let productos = JSON.parse(database.getItem(id))
+        const li = document.createElement("li")
+        li.textContent = `NOMBRE: ${productos.nombre} -DESCRIPCIÓN: ${productos.detalle} - PRECIO: ${productos.precio}`
+        ulProductos.appendChild(li)
+        const botonEliminarProductos = document.createElement("button")
+        botonEliminarProductos.textContent = "Borrar del carrito"
+        li.appendChild(botonEliminarProductos)
+        botonEliminarProductos.addEventListener ("click", () => {
+            eliminarProducto(database, productos)
+            listarProductos()
+        })
+    }
+}
+listarProductos()
